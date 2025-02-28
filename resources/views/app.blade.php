b909ff23c6e33869f72128c5077006fe4bd3ada6
Lumas <lumastech@gmail.com>
2025-02-28 21:02:23 +0000

    Move Res

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,12 +9,14 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+<!-- Link to Font Awesome CSS -->
+ <link rel="stylesheet" href="resources/assets/fa/all.min.css">
 
+ <!-- Link to Font Awesome JS -->
+ <script src="resources/assets/fa/all.min.js"></script>
         <!-- Scripts -->
         @routes
         @vite([
-            "resources/assets/fa/all.min.css",
-            "resources/assets/fa/all.min.js",
             'resources/js/app.js',
             "resources/js/Pages/{$page['component'
             ]}.vue"])
